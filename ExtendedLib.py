16d90f6870482eef53dc7553fc3d257108de39f6
Kyunianti19 <37916598+Kyunianti19@users.noreply.github.com>
2021-02-04 19:54:29 +0700

    Extended Vigenere

---
+++ ExtendedLib.py
@@ -0,0 +1,35 @@
+from VigenereLib import *
+
+def ExtendedEncrypt(plaintext,key):
+    # Extended Vigenere Encrypt plaintext with key
+    # Input : plaintext, key
+    # Output : ciphertext
+    
+    # Prepare the key
+    extended_key = GenerateVigenereKey(key,len(plaintext))
+
+    # Encrypt
+    result = ""
+    for i in range(len(plaintext)):
+        encrypted_char_num = (ord(plaintext[i]) + ord(extended_key[i]))%256
+        result += chr(encrypted_char_num)
+        if (i%5==4): # Set ciphertext to blocks of 5 characters
+            result += " "
+        
+    return result()
+    
+def ExtendedDecrypt(ciphertext,key):
+    # Extended Vigenere Decrypt ciphertext with key
+    # Input : ciphertext, key
+    # Output : plaintext
+    
+    # Prepare the key
+    extended_key = GenerateVigenereKey(key,len(ciphertext))
+    
+    # Decrypt
+    result = ""
+    for i in range(len(ciphertext)):
+        decrypted_char_num = (ord(ciphertext[i]) - ord(extended_key[i]))%256
+        result += chr(decrypted_char_num)
+        
+    return result
