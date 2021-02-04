5517f1fb7a6c5cbaf7bfb9ffae3aa8f78c97c4d7
Kyunianti19 <37916598+Kyunianti19@users.noreply.github.com>
2021-02-04 15:53:48 +0700

    Full Vigenere
    Full Vigenere

---
+++ Scratch.py
@@ -0,0 +1,8 @@
+from FullVigenereLib import *
+from VigenereLib import *
+
+A = FullVigenereEncrypt('beragam','aku')
+B = VigenereEncrypt('sayang','aku')
+C = FullVigenereDecrypt('DFHAXFK','aku')
+print(C)
+tes
